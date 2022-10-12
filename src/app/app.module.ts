82dca4c0db4b22abb48031d41c4c8a045c0bb146
Ann-Annice <annannice98@gmail.com>
2022-10-12 16:14:55 +0530

    userinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserinfoComponent
   ],
   imports: [
     BrowserModule,
